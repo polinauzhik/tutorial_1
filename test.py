203f3db55e870c47dfb74270d854e94ddb8cf4d9
Polina Uzhvak <uzhvak@istories.media>
2022-02-14 19:18:40 +0300

    Add input

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from datetime import datetime
-print("Hello!")
+name = input("What's your name? ")
+print("Hello, ", name,"!")
 print("We learn git")
 print("We learn GitHub")
 print("Our project: Git_Tutorial")
